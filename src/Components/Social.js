c60a8632a9155ac8723f3a330882990057da04be
MGcommits <marthagamez211@gmail.com>
2021-09-16 17:09:33 -0500

    Add resume link

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -2,30 +2,34 @@ import React, { Component } from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
 import { ImProfile } from "react-icons/im";
+import Resume from "../img/MarthaResume2021.pdf";
 class Social extends Component {
   render() {
     return (
-      <div className="social">
+      <div className="social icon">
         <a
           href="https://github.com/Martha121"
           target="_blank"
           rel="noopener noreferrer"
+          title="GitHub"
         >
           <AiFillGithub size="40px" color="orange" />
         </a>
         <a
-          className="icons"
+          className="icons icon"
           href="https://www.linkedin.com/in/martha-gamez-60a5764a/"
           target="_blank"
           rel="noopener noreferrer"
+          title="LinkedIn"
         >
           <AiFillLinkedin size="40px" color="orange" />
         </a>
         <a
-          className="icons"
-          href="https://www.linkedin.com/in/martha-gamez-60a5764a/"
+          className="icons icon"
+          href={Resume}
           target="_blank"
           rel="noopener noreferrer"
+          title="Resume"
         >
           <ImProfile size="40px" color="orange" />
         </a>
